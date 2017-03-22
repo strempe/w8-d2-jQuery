0b1e73d012c81c4db4e24613f6aa0ffaf1053751
Sara Trempe <88.trempe.sara@gmail.com>
2017-03-21 22:52:50 -0400

    INDEE air

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+       $('#items').load('nav.html', function() {
+       })
+
+       $('input').dateDropper();
+
+
+    $("input.label_better").label_better({
+        position: "top", // This will let you define the position where the label will appear when the user clicked on the input fields. Acceptable options are "top", "bottom", "left" and "right". Default value is "top".
+        animationTime: 500, // This will let you control the animation speed when the label appear. This option accepts value in milliseconds. The default value is 500.
+        easing: "ease-in-out", // This option will let you define the CSS easing you would like to see animating the label. The option accepts all default CSS easing such as "linear", "ease" etc. Another extra option is you can use is "bounce". The default value is "ease-in-out".
+        offset: 20, // You can add more spacing between the input and the label. This option accepts value in pixels (without the unit). The default value is 20.
+        hidePlaceholderOnFocus: true // The default placeholder text will hide on focus
+  });
+
+})
